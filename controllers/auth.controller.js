d1cda772e1628458a579759e4b2ee2e6af2d4154
Muhammed Aly <75846623+mohammedaly915@users.noreply.github.com>
2024-08-10 22:44:57 +0300

    add to print it in console

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,89 +1,100 @@
-const User = require("../models/User")
-const bcrypt=require("bcryptjs")
-const appError=require("../utiles/errors")
-const status= require("../utiles/status")
-const GenJWT=require("../utiles/generateJWT")
-const asyncWrapper = require("../middlewares/asyncWrapper")
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const appError = require("../utiles/errors");
+const status = require("../utiles/status");
+const GenJWT = require("../utiles/generateJWT");
+const asyncWrapper = require("../middlewares/asyncWrapper");
 
-const register= asyncWrapper(async(req,res,next)=>{
-    const {userName,email,password ,userCountry,userCity,userAddress,Gender}= req.body;
-    
-    let errors = [];
+const register = asyncWrapper(async (req, res, next) => {
+    try {
+        const { userName, email, password, userCountry, userCity, userAddress, Gender } = req.body;
 
-    if (!userName) {
-        errors.push("Username is required");
+        let errors = [];
+
+        if (!userName) errors.push("Username is required");
+        if (!email){
+            console.log("Email is required");
+            errors.push("Email is required");
+        }
+            
+        if (!password) {
+            console.log("password is required");
+            errors.push("Password is required");
+        }
+
+        if (errors.length > 0) {
+            throw appError.create(errors.join(", "), 400, status.FAIL);
+        }
+
+        const oldUser = await User.findOne({ email });
+        if (oldUser) {
+            console.log("موجود");
+            throw appError.create("User already exists", 400, status.FAIL);
+        }
+
+        const hashedPass = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            userName,
+            email,
+            password: hashedPass,
+            userPic: req.file.filename,
+            userCountry,
+            userCity,
+            userAddress,
+            Gender
+        });
+
+        const token = await GenJWT({ 
+            email: newUser.email,
+            id: newUser._id,
+            isAdmin: newUser.isAdmin 
+        });
+
+        await newUser.save();
+        console.log("done user");
+
+        res.status(201).json({ status: status.SUCCESS, data: newUser, token });
+
+    } catch (err) {
+        console.log("لو ده ظهر لغيني علطول يعم");
+        next(err);
     }
+});
 
-    if (!email) {
-        errors.push("Email is required");
+const login = asyncWrapper(async (req, res, next) => {
+    try {
+        const { userName, email, password } = req.body;
+
+        if ((!email && !userName) || !password) {
+            throw appError.create("Email or username and password are required", 400, status.FAIL);
+        }
+
+        let user;
+        if (email) {
+            user = await User.findOne({ email: email });
+        } else if (userName) {
+            user = await User.findOne({ userName: userName });
+        }
+
+        if (!user) {
+            throw appError.create("User not found", 400, status.FAIL);
+        }
+
+        const matchedPassWord = await bcrypt.compare(password, user.password);
+        if (matchedPassWord) {
+            const token = await GenJWT({ email: user.email, id: user._id, isAdmin: user.isAdmin });
+            return res.json({ status: "Success", data: { user }, token });
+        } else {
+            throw appError.create("Password is wrong", 400, status.FAIL);
+        }
+
+    } catch (err) {
+        console.log("لو ده ظهر لغيني علطول يعم");
+        next(err);
     }
+});
 
-    if (!password) {
-        errors.push("Password is required");
-    }
-
-    if (errors.length > 0) {
-        const error = appError.create(errors.join(", "), 400, status.FAIL);
-        return next(error);
-    }
-    const oldUser= await User.findOne({email})
-    if (oldUser){
-        const error = appError.create("user alreay exist",400,status.FAIL);
-          return next(error);
-    }
-    const hashedPass = await bcrypt.hash(password, 10);
-    const newUser = new User({
-        userName,
-        email,
-        password: hashedPass,
-        userPic:req.file.filename,
-        userCountry,
-        userCity,
-        userAddress,
-        Gender
-      });
-    
-    //genarate token
-    const token = await GenJWT({   email: newUser.email,
-                                        id: newUser._id,
-                                        isAdmin:newUser.isAdmin});
-    //newUser.token = token;
-
-    // save in database
-    await newUser.save();
-    res.status(201).json({ status: status.SUCCESS, data:newUser,token });
-}) 
-
-const login =asyncWrapper(async(req,res,next)=>{
-    const {userName,email,password}=req.body;
- 
-    if ( (!email && !userName) && !password ) {
-        const error = appError.create("Email or username and password are required", 400, status.FAIL);
-        return next(error);
-    }
-    let user;
-    if (email) {
-        user = await User.findOne({ email: email });
-    } 
-    else if (userName) {
-        user = await User.findOne({ userName: userName });
-    }    
-    if (!user) {
-        const error = appError.create("user not found", 400, status.FAIL);
-        return next(error);
-      }
-    const matchedPassWord=await bcrypt.compare(password,user.password)
-    if (user&&matchedPassWord){
-        const token = await GenJWT({ email: user.email, id: user._id, isAdmin: user.isAdmin });
-        //user.token=token
-        return res.json({status:"Success",data:{user},token})
-    } 
-    if (!matchedPassWord)return res.json("password is wrong")
-    
-})
-
-
-module.exports={
+module.exports = {
     register,
     login
-} 
+};
